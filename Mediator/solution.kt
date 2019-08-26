8c2d18c12b18f176c6ccbffde5f82be4fd6cad1b
Julian Crouch <jcrouch@riskalyze.com>
2019-08-26 08:29:30 -0700

    adds mediator pattern

---
+++ Mediator/solution.kt
@@ -0,0 +1,69 @@
+interface Command {
+    fun land()
+}
+
+interface IATCMediator {
+    val canHazLandOkay: Boolean
+
+    fun registerYerselfARunway(runway: Runway)
+    fun registerYerselfAFlight(flight: Flight)
+    fun setLandingStatus(status: Boolean)
+}
+
+class ATCMediator: IATCMediator {
+    private val flight: Flight? = null
+    private val runway: Runway? = null
+
+    override val canHazLandOkay = false
+
+    override fun registerYerselfARunway (runway: Runway) {
+        this.runway = runway
+    }
+
+    override fun registerYerselfAFlight (flight: Flight) {
+        this.flight = flight
+    }
+
+    override fun setLandingStatus (status: Boolean) {
+        canHazLandOkay = status
+    }
+}
+
+class Runway (private val atcMediator: IATCMediator) : Command {
+    init {
+        atcMediator.setLandingStatus(true)
+    }
+
+    override fun land() {
+        println("You can haz landing")
+        atcMediator.setLandingStatus(true)
+    }
+}
+
+class Flight(private val atcMediator: IATCMediator) : Command {
+    override fun land() {
+        if (atcMediator.canHazLandOkay) {
+            println("Landing Done")
+            atcMediator.setLandingStatus(true)
+        } else {
+            println("gotta wait bruh"
+        }
+    }
+
+    fun getReady() {
+        println("gettnin rdy")
+    }
+}
+
+fun main (args: Array<String>) {
+    val atcMediator = ATCMediator()
+    val stupidPlane = Flight(atcMediator)
+    val aRunway = Runway(atcMediator)
+    atcMediator.registerYerselfAFlight(stupidPlane)
+    atcMediator.registerYerselfARunway(aRunway)
+
+    stupidPlane.getReady()
+    aRunway.land()
+    stupidPlane.land()
+}
+
